e9598541ed3144f8f4cd105272fc24944a3c94d2
JohnJAF <John303goty@gmail.com>
2026-06-08 15:01:03 -0600

    added a gitignore so we dont commit .txt files while demoing. Then i finished the azlyrics scrape function by cleaning up the lyrics as to ONLY grab lyrics

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 from bs4 import Comment
 # parsing for links to make sure links are links
 from urllib.parse import urlparse
+# regex parsing
+import re
 
 # function for grabbing web scrape from azlyrics
 def azLyricScrape(link):
@@ -18,15 +20,41 @@ def azLyricScrape(link):
     soup = BeautifulSoup(response.text, 'html.parser')
     
     comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+    
+    textFromWebsite = ""
+    
+    # finding the div where the actual lyrics are (because its unmarked we have to go by comment and this comment shows up every time over the lyrics)
     for comment in comments:
         if "Usage of azlyrics.com content" in comment:
-            print(comment.parent.prettify())
+            textFromWebsite = comment.parent.text.strip() # this skips all the html bs and gets ONLY the text
+            
+    # now that we have the actual lyrics, lets parse the lyrics so we can genuinely get what we need.
+    lyricsOnly = re.sub("\\[.+?\\]", "", textFromWebsite) # .+? is telling regex "any block of text with any thing in between [ and ] is fair game to remove"
+    
+    # print(lyricsOnly)
+    
+    listOfLines = lyricsOnly.splitlines() # turns string into list of lines
+    listOfLinesNoBlanks = [] 
+    
+    # i have to do this bs to remove blanks bc splitlines doesnt just skip blanks 
+    
+    with open("lyricsholder.txt", "w") as f:
+
+        for line in listOfLines:
+            if line == "":
+                continue
+            else:
+                f.write(line + "\n")
+                continue
+        
+    
     
 # grab da link
 def grabDaLink():
     linkString = ""
     while(True):
         linkString = input("What link would you like to use? \n").lower().strip()
+        print("\n \n")
         
         linkObject = urlparse(linkString) # this should turn link into a urlparse object which would allow us to actually parse it as a URL
         
@@ -36,7 +64,7 @@ def grabDaLink():
             response = requests.get(linkString)
             
             if response.status_code == 200: # 200 = successful website request
-                print("this link si real and the website exists")
+                #print("this link si real and the website exists")
                 return linkString
             else:
                 print("this link does not exist")
@@ -52,6 +80,10 @@ def grabDaLink():
     # main will look at such txt and print it out
 def main():
     link = grabDaLink()
+    
+    # i could probably add some like link checker so for ex: if its a azlyrics link then we go to that fucntion
+    # if we figure genius out we could go there too
+    # if its an apple or spotify api we can figre that out IDK
     
     azLyricScrape(link)
     
